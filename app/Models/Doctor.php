e0728d4f0ab75f0cb23117daaa344851890f87cc
abdulwhab241 <abdulwhab241@gmail.com>
2023-09-25 23:38:42 -0400

    add resources

---
+++ app/Models/Doctor.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Section;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Doctor extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+    
+    protected $fillable = [
+        'name',
+        'phone',
+        'status',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'image' => 'array',
+    ];
+
+    public function section()
+    {
+        return $this->belongsTo(Section::class,'section_id');
+    }
+}
